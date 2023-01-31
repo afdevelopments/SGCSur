03e26993c1a76ef49dbb084dcf634a1b8bff801f
Fernando Chavez <fernandochajim@gmail.com>
2023-01-31 13:58:45 -0600

    logos y script de cargado de página

--- wingoapp/static/assets/js/notify/index.js
+++ wingoapp/static/assets/js/notify/index.js
@@ -1,5 +1,5 @@
 'use strict';
-var notify = $.notify('<i class="fa fa-bell-o"></i><strong>Loading</strong> page Do not close this page...', {
+var notify = $.notify('<i class="fa fa-bell-o"></i><strong>Cargando</strong> No cierre esta página...', {
     type: 'theme',
     allow_dismiss: true,
     delay: 2000,
@@ -12,5 +12,5 @@ var notify = $.notify('<i class="fa fa-bell-o"></i><strong>Loading</strong> page
 });
 
 setTimeout(function() {
-    notify.update('message', '<i class="fa fa-bell-o"></i><strong>Loading</strong> Inner Data.');
+    notify.update('message', '<i class="fa fa-bell-o"></i><strong>Cargando</strong> Datos Internos.');
 }, 1000);
