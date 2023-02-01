e091b1e77884cf974a40c707146dcd86cb3bffa7
César Ramírez <cesarpotter7@gmail.com>
2023-02-01 15:04:21 -0600

    Venv eliminado y requeriments agregado.

--- sistemita-env/Scripts/activate.bat
+++ sistemita-env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Projects\sistemita\sistemita-env
+set VIRTUAL_ENV=C:\Users\cesar\OneDrive\Documentos\Cesar\AFDevelopment\Proyecto de Vinculación\Sistema\sistemita\sistemita-env
 
 if not defined PROMPT set PROMPT=$P$G
 
