0a1fb3b7f61a4e9c8ce7a34d6c06546c2e5fef68
Fernando Chavez <fernandochajim@gmail.com>
2023-02-18 14:46:18 -0600

    Update forms.py

--- wingoapp/forms.py
+++ wingoapp/forms.py
@@ -16,6 +16,7 @@ class TaskForm(forms.ModelForm):
 
 # Para cambiar el label del usuario y contraseña
 class CustomAuthenticationForm(AuthenticationForm):
+
     username = UsernameField(
         label='Usuario',
         widget=forms.TextInput()
@@ -23,7 +24,7 @@ class CustomAuthenticationForm(AuthenticationForm):
     password = forms.CharField(label="Contraseña", widget=forms.PasswordInput)
 
 
-# Formulario de añadir carrera
+# Formulario de añadir / modificar carrera
 class CarreraForm(ModelForm):
     nombreCarrera = forms.CharField(max_length=50, label="Nombre de la carrera")
     divisionesMenu = [
@@ -34,12 +35,13 @@ class CarreraForm(ModelForm):
     divisionCarrera = forms.CharField(max_length=100, label="División de la carrera",
                                       widget=forms.Select(choices=divisionesMenu))
 
+    # Class Meta para definir el modelo y los campos que se van a mostrar
     class Meta:
         model = Carreras
         fields = ['nombreCarrera', 'divisionCarrera']
 
 
-# Formulario de añadir empresa
+# Formulario de añadir / modificar empresa
 class EmpresaForm(ModelForm):
     razonSocial = forms.CharField(max_length=200, label="Nombre de la empresa")
     rfc = forms.CharField(max_length=13, label="RFC de la empresa")
@@ -53,6 +55,20 @@ class EmpresaForm(ModelForm):
     sectorEmpresa = forms.CharField(max_length=100, label="Sector de la empresa",
                                     widget=forms.Select(choices=sectoresMenu))
 
+    # Class Meta para definir el modelo y los campos que se van a mostrar
     class Meta:
         model = Empresa
         fields = ['razonSocial', 'rfc', 'giro', 'sectorEmpresa']
+
+
+# Formulario de añadir / modificar contacto
+class ContactoForm(ModelForm):
+    nombre = forms.CharField(max_length=50, label="Nombre del contacto")
+    numTelefono = forms.CharField(max_length=10, label="Número de teléfono")
+    idEmpresa = forms.ModelChoiceField(queryset=Empresa.objects.all(), label="Empresa")
+
+
+    # Class Meta para definir el modelo y los campos que se van a mostrar
+    class Meta:
+        model = Contacto
+        fields = ['nombre', 'numTelefono', 'idEmpresa']
